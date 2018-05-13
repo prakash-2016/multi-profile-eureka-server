02ec23f5dc638715d601e2d1067d752d70b3f5aa
prakash-2016 <prakash.chandra.mailbox@gmail.com>
2018-05-13 13:11:22 +0530

    Initial commit

---
+++ multiple-cloud-server/src/main/java/com/cloud/server/MultipleCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class MultipleCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleCloudServerApplication.class, args);
+	}
+}
